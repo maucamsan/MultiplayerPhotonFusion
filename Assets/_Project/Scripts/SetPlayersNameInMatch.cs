fb04d74ae01a69728f760ebbd3150f5f3e080b81
maucamsan <47923785+maucamsan@users.noreply.github.com>
2024-03-21 11:52:14 -0500

    Change testing for usernames display

--- Assets/_Project/Scripts/SetPlayersNameInMatch.cs
+++ Assets/_Project/Scripts/SetPlayersNameInMatch.cs
@@ -2,16 +2,19 @@ using Fusion;
 using UnityEngine;
 using TMPro;
 
-public class SetPlayersNameInMatch : NetworkBehaviour, IPlayerJoined
+public class SetPlayersNameInMatch : NetworkBehaviour
 {
     [SerializeField] TMP_Text playersNameDisplay;
     [Networked, OnChangedRender(nameof(NameSet))]
     public string PlayersName {get; set;}
     public StringVariable stringVariable;
+    private bool hasNameSet;
     
     void OnEnable()
     {
         PlayerSpawner.OnNewPlayerJoined += UpdateOnNewPlayer;
+        playersNameDisplay.text = stringVariable.UserName;
+        UpdateOnNewPlayer();
     }
 
     void OnDisable()
@@ -22,16 +25,13 @@ public class SetPlayersNameInMatch : NetworkBehaviour, IPlayerJoined
 
     private void UpdateOnNewPlayer()
     {
-        if (HasStateAuthority)
+        if (HasStateAuthority && !hasNameSet)
             PlayersName = stringVariable.UserName;
+        hasNameSet = true;
     }
     public void NameSet()
     {
         playersNameDisplay.text = PlayersName;
     }
-
-    public void PlayerJoined(PlayerRef player)
-    {
-        playersNameDisplay.text = stringVariable.UserName;
-    }
+    
 }
